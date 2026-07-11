d037db2ba0ddb0241a536fab6b58150ee61cfb23
Arthur Leung <1808arthur@gmail.com>
2026-07-11 12:47:37 -0300

    Completed 'Hello World'

---
+++ HelloDotNet/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Arthur";
+
+Console.WriteLine($"Hello, {name}!");
+
+DateTime today = DateTime.Today;
+DateTime nextChristmas = new DateTime(today.Year, 12, 25);
+
+if (today > nextChristmas)
+{
+    nextChristmas = new DateTime(today.Year + 1, 12, 25);
+}
+
+int daysUntilChristmas = (nextChristmas - today).Days;
+
+Console.WriteLine($"There are {daysUntilChristmas} days until Christmas.");
